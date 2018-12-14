ae7af842eaa57116071bed7e220b212c5623356a
wgocalinska <224076@edu.p.lodz.pl>
2018-12-14 15:45:38 +0100

    chart-14,12

---
+++ chart5.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import linregress as lr
+
+def plot():
+    file = input('Enter your file : ')
+
+    x, y = np.loadtxt(file, delimiter=',', unpack=True, skiprows=1)
+
+    a, b, r, _, da = lr(x, y)
+    regline = a*x + b
+
+    print('Regression line = ' + str(round(a, 2)) + '*x + ' + str(round(b, 2)))
+    print('The Pearson correlation coefficient = ' + str(round(r, 2)))
+    print('The standard deviation =' + str(round(da, 2)))
+
+    plt.figure(figsize=(5, 5))
+    plt.plot(x, y, 'ro')
+    plt.plot(x, regline)
+    plt.grid(True)
+    plt.xlabel("Mass [g]")
+    plt.ylabel("Elongate [cm]")
+    plt.tight_layout(0,2)
+    plt.show()
+plot()
